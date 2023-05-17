d677c9094d022b346903db5ff85ba6f0bb07f11e
Ge Su <gegege.su@gmail.com>
2023-05-17 23:44:02 +1000

    instal dependencies and added index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,109 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const { title } = require('process');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const gernerateREADME = ({ github, email, title, description, installation, usage, license, contributing, tests }) =>
+`# ${title}
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+## Description
+${description}
 
-// TODO: Create a function to initialize app
-function init() {}
+## Table of Content
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+To install the neccessary dependencies, run the following command:
+
+${installation}
+
+## Usage
+${usage}
+
+## License
+This projece is licensed under the ${license} license.
+
+## Contributing
+${contributing}
+
+## Tests
+To run tests, run the following command:
+
+${tests}
+
+## Questions
+If you have any questions about the repo, open an issue or contact me directly at [${email}](${email}). You can find more of my work at [${github}](https://github.com/${github}).`;
+
+// Create an array of questions for user input
+const questions = [{
+    type: 'input',
+    name: 'github',
+    message: 'What is your Github username?',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?',
+  },
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is your project title?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Please write a short description of your project?',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'What command should be run to install dependencies?',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'What does the user need to know about using the repo?',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'What kind of license should your project have?',
+    choices: ['MIT', 'APACHE_2.0', 'GPL_3.0', 'BSD_3', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'What is the user contribution guidelines?',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'What command should be run to run tests?',
+  },
+];
+
+// Create a function to write README file
+function writeToFile(readmeContent) {
+    fs.writeFile('README.md', readmeContent, (err) =>
+      err ? console.log(err) : console.log('README.md created successfully!')
+    );
+}
+
+// Create a function to initialize app
+function init() {
+    inquirer
+    .prompt(questions)
+    .then((answers) => {
+        const readmeContent = gernerateREADME(answers);
+        writeToFile(readmeContent);
+  });
+}
 
 // Function call to initialize app
 init();
